b982b1365d4d529593d4e7c9695486a441427b6f
CrashMTB <berglunddayne@gmail.com>
2024-09-24 09:48:03 -0500

    24-24 class

---
+++ unit 2 - functions and variables/input_practice-9-24-24.py
@@ -0,0 +1,11 @@
+#user_input = input("Enter something: ")         #allows you to write something in the terminal before the next line of code is ran
+#print("You entered: " + user_input)
+#input("somthing")           #allows you to say a question and an type dont use
+
+#potato = input("enter a number\n>")         #you write in a  new line of code
+#potato = int(potato)            #turns the string into a integer
+#print(potato * 10)          #multiply potato by 10, doesnt work if you put in none integers 
+print("welcome to WORD SMASH!")
+potao = input("enter word one\n>")
+potato = input("word two\n>")
+print(potao + potato)
---
+++ unit 2 - functions and variables/practice_9-24.py
@@ -0,0 +1,7 @@
+my_number = 4           #Declaration: creating
+print(my_number)
+my_number = 5           #Assignment: changing 
+print(my_number)
+print("my number") 
+my_other_number = 2 + my_number
+print(my_other_number)          #the stuff inside the() are peramiters
