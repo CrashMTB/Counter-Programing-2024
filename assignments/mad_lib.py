38ce82ba7549163c34b1c00ad57d3e3cbaeeed60
CrashMTB <berglunddayne@gmail.com>
2024-09-25 09:30:18 -0500

    animalsurvey_not_added

---
+++ assignments/mad_lib.py
@@ -0,0 +1,6 @@
+#once apon a time an (crearure0) named (name1) 
+#this (creature0) lived in a (object)
+#then one day a (adjective) and (adjective 2) (creature1) destoryed (city)
+#but the (adjective3) (creature0) named (name1) saved the city
+#and all the other (creature0)s in (city) cheered with (emotion) 
+#at the end of the the day (name1) partied with (celeberty) all night long
