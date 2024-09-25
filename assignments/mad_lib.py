6ec3bff8ba6a3f7708e51ca133987bfa5da9e6ab
CrashMTB <berglunddayne@gmail.com>
2024-09-25 09:51:20 -0500

    data transfer practice

--- assignments/mad_lib.py
+++ assignments/mad_lib.py
@@ -4,3 +4,14 @@
 #but the (adjective3) (creature0) named (name1) saved the city
 #and all the other (creature0)s in (city) cheered with (emotion) 
 #at the end of the the day (name1) partied with (celeberty) all night long
+#the end
+creature = input("name a random creature/animal\n> ")
+name = input("enter a name for the main charecter\n> ")
+object = input("give me any object\n> ")
+adjective = input("give me a random adjective \n> ")
+adjective2 = input("give me a random adjective \n> ")
+creature2 = input("give me a mythical creature\n> ")
+city = input("what is the name of a city\n> ")
+creature3 = input("give me a mythical creature\n> ")
+emotion = input("name any stong emotion\n> ")
+celeberty = ("name every celeberty")
