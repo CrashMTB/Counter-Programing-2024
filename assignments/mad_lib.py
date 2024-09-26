56c556c4ce96d5c1282dd8d992de78129a99468b
CrashMTB <berglunddayne@gmail.com>
2024-09-26 17:33:12 -0500

    mad_lib home revisory 9/26/24

--- assignments/mad_lib.py
+++ assignments/mad_lib.py
@@ -3,15 +3,15 @@
 #then one day a (adjective) and (adjective 2) (creature1) destoryed (city)
 #but the (adjective3) (creature0) named (name1) saved the city
 #and all the other (creature0)s in (city) cheered with (emotion) 
-#at the end of the the day (name1) partied with (celeberty) all night long
-#the end
-creature = input("name a random creature/animal\n> ")
-name = input("enter a name for the main charecter\n> ")
-object = input("give me any object\n> ")
-adjective = input("give me a random adjective \n> ")
-adjective2 = input("give me a random adjective \n> ")
-creature2 = input("give me a mythical creature\n> ")
-city = input("what is the name of a city\n> ")
-creature3 = input("give me a mythical creature\n> ")
-emotion = input("name any stong emotion\n> ")
-celeberty = input("name every celeberty")
+#at the end of the the day (name1) partied with (celeberty) all night long the end
+creature0 = input("Name a random creature/animal?\n> ")                   # lines 7-16 are user input codes to find the answers for the madlib awnsers
+name1 = input("Enter a name for the main charecter?\n> ")
+object = input("Give me any object?\n> ")
+adjective = input("Give me a random adjective? \n> ")
+adjective2 = input("Give me a new random adjective? \n> ")
+creature1 = input("Give me a mythical creature?\n> ")
+city = input("What is the name of a city?\n> ")
+adjective3 = input("Give me a new random adjective? \n> ")
+emotion = input("Name any stong emotion?\n> ")
+celeberty = input("Name any celeberty?\n> ")
+print("Once apon a time there was a " + creature0 +" named " + name1 + ", and " + name1 + " lived in a " + object + ". Then one day a " + adjective + " and " + adjective2 + " " + creature1 + " destroyed " + city + ".\nBut the " + adjective3 + " " + creature0 + " named " + name1 + " saved " + city + ", and all the other " + creature0+ "'s in " + city + " cheered with " + emotion + ". \nAt the end of the the day, " + name1 + " partied with " + celeberty + " all night long.\nTHE END")
