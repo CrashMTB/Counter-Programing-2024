48260c214389aed777f6cbfdf437e24ce2efc395
CrashMTB <berglunddayne@gmail.com>
2024-09-30 09:53:58 -0500

    in class 9-30

---
+++ assignments/basic clculater.py
@@ -0,0 +1,13 @@
+def add():
+    print("add two numbers")
+    x = input ("what would is the first number you want to add\n>")
+    x = int(x)
+    y = input ("what would is the second number you want to add\n>")
+    y = int(y)
+    print(str(x) + " + " + str(y) + " = " + str(x + y)) 
+add()
+
+
+
+
+
---
+++ unit 2 - functions and variables/functionpractice.py
@@ -0,0 +1,5 @@
+def calculate_tax (item, price, rate):
+    print (item)
+    print (price * rate)
+    return price * rate
+calculate_tax ("potato", 17, 0.06875)
