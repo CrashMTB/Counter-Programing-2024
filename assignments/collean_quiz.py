8c3055be1658884fe8dab8beb3b20df118b2e5df
CrashMTB <berglunddayne@gmail.com>
2024-10-04 09:44:42 -0500

    10/4/2024 in calss boolean quiz

---
+++ assignments/collean_quiz.py
@@ -0,0 +1,20 @@
+q1 = int(input("give me a number less than 999?\n> "))
+print(q1 < 999)
+q2 = int(input("give me a number more than 999?\n> "))
+print(q2 > 999)
+q3 = int(input("give me a number equal too 666?\n>  "))
+print(q3 == 666)
+q4 = int(input("give me a number less than or equal too 666?\n>  "))
+print(q4 <= 666)
+q5 = int(input("give me a number less than 999?\n>  "))
+print(q4 > 666)
+
+
+q6 = int(input("give me a number that  is equal to 7 or that is smaller than 24?\n> "))
+print(q6 == 7 or q6 < 24)
+
+q7 = int(input("give me a number that  is equal to 7 and that is smaller than 24?\n> "))
+print(q7==7 or q7 < 24)
+
+q8 = int(input("give me a number that is less than 999 but more than 722?\n> "))
+print(q8<999 and q8>722)
---
+++ unit 3 - Conditions and Exceptions/boolean expresion practice.py
@@ -0,0 +1,65 @@
+print(5 > 10)
+# values: 5, 10
+# realtional operator: >
+
+
+x = "blue"
+print(x == "yellow")
+# values: x, "yellow"
+# realtional operator: ==
+
+
+answer = "cube"
+count = 1
+print(answer == "cube" and count < 10)
+# values: answer, "cube", count, 10
+# realtional operators: ==, <
+# logical operators: and
+
+temperature = 81
+if (temperature > 80):
+  print("It's a hot day!")
+
+
+
+  temperature = 79
+if (temperature > 80):
+    print("It's a hot day!")
+else:
+    print("It's not a hot day.")
+
+
+
+
+    temperature = 79
+if (temperature > 80):
+    print("It's a hot day!")
+elif (temperature >= 70):
+    print("It's a warm day.")
+else:
+    print("It's a cool day.")
+
+
+
+def weather():
+    temp = int(input("what is the tempature outside today?\n> "))
+    temp = int(temp)
+    if (temp > 80):
+        print("the sun is cooking you alive!!!")
+    elif (temp > 70):
+        print("its beutiful outside")
+    else: 
+        print("have some coco its a bit cold out, sit by the fire, and watch a movie")
+weather()
+
+
+
+
+print(4 >= 2)
+print(1993 == 1993)
+print(-90 < -99)
+print(10!= 10)
+print(4 > 3 and 3 > 2)
+
+
+
