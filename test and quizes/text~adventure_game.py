f1e67d9568fe5925a2daeadb883976b2bf3ad007
CrashMTB <berglunddayne@gmail.com>
2024-10-25 09:54:22 -0500

    fiw it

--- test and quizes/text~adventure_game.py
+++ test and quizes/text~adventure_game.py
@@ -1,20 +1,21 @@
 def start(): #this is how i start the program and reset it after an ending
     print("Just 5 seconds ago you were crossing the road when a truck ran you over, as your ability to feel pain and discomfort disapates,and your eyes start to close you here a voice") #start of an advadture
+    second_chance()
 def restart():                         # use this function at the end of a path to see if the user would like to play again
-    restart = input("Would you like to restart?\n> ").lower
-    if restart == "yes":
+    restar = input("Would you like to restart?\n> ").lower
+    if restar == "yes":
         start()
-    elif restart == "no":
+    elif restar== "no":
         print("Good bye")
     else:
         print("Not valid")
         restart()
 def second_chance():             #section 1
-    do_you_live = input("Hello there child. You can have a second chance at life. Will you take your second chance? (yes/no) \n> ").lower
-    if do_you_live == "yes":
+    do_you_live = input("Hello there child. You can have a second chance at life. Will you take your second chance? \n1. yes\n2. no \n>")
+    if do_you_live == "1":
         print("Good i will give you a second chance")
         you_do()
-    elif do_you_live == "no":
+    elif do_you_live == "2":
         you_dont()
     else:
         print("That isnt valid, try again")
@@ -33,13 +34,13 @@ def you_dont():                  #section 1 path 1           ending 1
         print("Invalid try again")
         you_dont()
 def you_do():                    #section 2 path 2 
-    print("Suddenly as your senses come back you start to feel something around you like your rapped in something, you open your eyes to realize your in the arms of a young woman whos looking down at you.")
+    print("Suddenly as your senses come back you start to feel something around you like your wrapped in something, you open your eyes to realize your in the arms of a young woman whos looking down at you.")
     print("A man enters a room and says, so thats our baby son. In a moment of relization you dont know what to do")
     baby = input("You are shocked and cant think of what to do so do you 1. do nothing, or 2. cry?\n> ")
-    if baby == "1": 
+    if baby == "2": 
         print("Your new mother starts to rock you back and forth trying to sooth you.")
         sooth()
-    elif baby == "2":
+    elif baby == "1":
         print("As you stare off into nothingness you new father leans down and says i'm sure you will be a fine heir.")
         heir_time_skip()
     else:
@@ -47,13 +48,13 @@ def you_do():                    #section 2 path 2
         you_do()
 def sooth():                     #section 3 path 1           ending 2
     print("As you are soothed you feel much better")
-    to_mother = input("you feel a lot better so will you 1. stop crying, 2, say thank you, or 3.continue to cry")
+    to_mother = input("you feel a lot better so will you 1. stop crying, 2, say thank you, or 3.continue to cry\n>")
     if to_mother == "1":
         print("Thats it dear. ive got you its ok")
         mom_timeskip()
     elif to_mother == "2":                                                                                                              #ending 2
-        print("you imedeatly relize your mistake as you are thrown into the forest to die as the thought you were possesed by a demon.")
-        print("it seams this place is primitive in there thinking, probably midieval,")
+        print("you immeadetyl relize your mistake as you are thrown into the forest to die as the thought you were possesed by a demon.")
+        print("it seams this place is primitive in there thinking, probably medieval,")
         print("Demon? ending")
         restart()
     elif to_mother == "3":
@@ -198,3 +199,4 @@ def accept():                    #section 6 path 4             ending 10
     print("You decided that it wasnt worth the fight to get out and you never did get out you ended up dying of old age in there.")
     print("Dungeon. ending")      #ending 10
     restart()
+start()
